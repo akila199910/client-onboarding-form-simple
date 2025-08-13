efd6638dfc071516b625fe1ad2bd4bb1637f041d
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-08-13 13:12:11 +0530

    topic dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,15 @@ export default function Page() {
 
   return (
     <main className="mx-auto max-w-3xl p-4 sm:p-6 lg:p-10">
-      <p className="text-2xl caret-amber-300 text-amber-200">This is main page</p>
+
+      {/* topic dev */}
+      <div className="mx-auto mb-6 text-center">
+        <h1 className="text-2xl font-bold tracking-tight sm:text-3xl">Client Onboarding</h1>
+        <p className="mt-1 text-sm text-gray-600 font-semibold">Technical Assessment: Client Onboarding (Next.js
+          + React Hook Form + Zod)</p>
+      </div>
+
+      
     </main>
   );
 }
