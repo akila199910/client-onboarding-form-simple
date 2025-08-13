de6e91ced31be03031ae68d6651a68ae79698c1a
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2025-08-13 15:53:41 +0530

    create the full form without serviced

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client"
 
-import FormField from "@/app/components/FormField";
-import ServicesCheckboxes from "@/app/components/ServicesCheckboxes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { OnboardInput, OnboardSchema } from "./lib/validation";
 
 
 
 export default function Page() {
+
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
+    resolver: zodResolver(OnboardSchema),
+    mode: "onBlur"
+  });
+
+  const onsubmit = async (formData:OnboardInput) => {
+    console.log(formData)
+  }
 
 
   return (
@@ -20,99 +30,104 @@ export default function Page() {
 
       {/* form div */}
 
-      <form  className="grid gap-4 rounded-2xl border bg-white p-4 shadow-sm sm:p-6">
+      <form onSubmit={handleSubmit(onsubmit)} className="grid gap-4 rounded-2xl border bg-white p-4 shadow-sm sm:p-6">
 
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                <div className="space-y-1">
-                  <label htmlFor="fullName" className="block text-sm font-medium"> Full Name</label>
-                    <input
-                      id="fullName"
-                      type="text"
-                      autoComplete="name"
-                      className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
-                    />
-                  <p role="alert" className="text-sm text-red-600">This is error</p>
-                </div>
-
-                <div className="space-y-1">
-                  <label htmlFor="email" className="block text-sm font-medium"> Email</label>
-                    <input
-                      id="email"
-                      type="email"
-                      autoComplete="email"
-                      className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
-                    />
-                  <p role="alert" className="text-sm text-red-600">This is error</p>
-                </div>
-            </div>
-             
-            <div className="space-y-1">
-              <label htmlFor="email" className="block text-sm font-medium"> Company name</label>
-                <input
-                  id="companyName"
-                  type="text"
-                  autoComplete="email"
-                  className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
-                />
-              <p role="alert" className="text-sm text-red-600">This is error</p>
-            </div>
-
-            <div>
-              <label className="mb-1 block text-sm font-medium">Services interested in</label>
-              <div className="grid grid-cols-2 gap-2 sm:grid-cols-4">
-                <label  className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
-                  <input type="checkbox"  className="h-4 w-4" />
-                  <span className="text-sm">UI/UX</span>
-                </label>
-
-                <label  className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
-                  <input type="checkbox"  className="h-4 w-4" />
-                  <span className="text-sm">Branding</span>
-                </label>
-                <label  className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
-                  <input type="checkbox"  className="h-4 w-4" />
-                  <span className="text-sm">Web Dev</span>
-                </label>
-                <label  className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
-                  <input type="checkbox"  className="h-4 w-4" />
-                  <span className="text-sm">Mobile App</span>
-                </label>
-                <p role="alert" className="col-span-full text-sm text-red-600">Error</p>
-              </div>
-            </div>
-            
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">            
-              <div className="space-y-1">
-                <label htmlFor="budgetUsd"  className="block text-sm font-medium"> Budget (USD)</label>
-                  <input
-                    id="budgetUsd"
-                    type="number"
-                    inputMode="numeric" 
-                    min={100}
-                    max={1000000}
-                    className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
-                  />
-                <p role="alert" className="text-sm text-red-600">This is error</p>  
-              </div>
-
-              <div className="space-y-1">
-                <label htmlFor="budgetUsd"  className="block text-sm font-medium">Project start date</label>
-                  <input
-                    id="projectStartDate"                    
-                    type="date"
-                    className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
-                  />
-                <p role="alert" className="text-sm text-red-600">This is error</p>
-              </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div className="space-y-1">
+            <label htmlFor="fullName" className="block text-sm font-medium"> Full Name</label>
+            <input
+              id="fullName"
+              type="text"
+              autoComplete="name"
+              className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
+              {...register("fullName")}
+            />
+            <p role="alert" className="text-sm text-red-600">This is error</p>
           </div>
 
-          <label className="flex items-center gap-2">
-            <input type="checkbox" className="h-4 w-4"  />
-            <span className="text-sm">I accept the terms</span>
-          </label>
+          <div className="space-y-1">
+            <label htmlFor="email" className="block text-sm font-medium"> Email</label>
+            <input
+              id="email"
+              type="email"
+              autoComplete="email"
+              className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
+              {...register("email")}
+            />
+            <p role="alert" className="text-sm text-red-600">This is error</p>
+          </div>
+        </div>
+
+        <div className="space-y-1">
+          <label htmlFor="companyName" className="block text-sm font-medium"> Company name</label>
+          <input
+            id="companyName"
+            type="text"
+            className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
+            {...register("companyName")}
+          />
+          <p role="alert" className="text-sm text-red-600">This is error</p>
+        </div>
+
+        <div>
+          <label className="mb-1 block text-sm font-medium">Services interested in</label>
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-4">
+
+            {/* <label className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
+              <input type="checkbox" className="h-4 w-4" value= 'UI/UX' {...register()} />
+              <span className="text-sm">UI/UX</span>
+            </label>
+
+            <label className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
+              <input type="checkbox" className="h-4 w-4" />
+              <span className="text-sm">Branding</span>
+            </label>
+            <label className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
+              <input type="checkbox" className="h-4 w-4" />
+              <span className="text-sm">Web Dev</span>
+            </label>
+            <label className="flex items-center gap-2 rounded-lg border p-2 hover:bg-gray-50">
+              <input type="checkbox" className="h-4 w-4" />
+              <span className="text-sm">Mobile App</span>
+            </label>
+            <p role="alert" className="col-span-full text-sm text-red-600">Error</p> */}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div className="space-y-1">
+            <label htmlFor="budgetUsd" className="block text-sm font-medium"> Budget (USD)</label>
+            <input
+              id="budgetUsd"
+              type="number"
+              inputMode="numeric"
+              min={100}
+              max={1000000}
+              className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
+              {...register("budgetUsd")}
+            />
+            <p role="alert" className="text-sm text-red-600">This is error</p>
+          </div>
+
+          <div className="space-y-1">
+            <label htmlFor="budgetUsd" className="block text-sm font-medium">Project start date</label>
+            <input
+              id="projectStartDate"
+              type="date"
+              className="h-10 w-full rounded-lg border px-3 outline-none focus:ring-2"
+              {...register("projectStartDate")}
+            />
+            <p role="alert" className="text-sm text-red-600">This is error</p>
+          </div>
+        </div>
+
+        <label className="flex items-center gap-2">
+          <input type="checkbox" className="h-4 w-4" {...register("acceptTerms")} />
+          <span className="text-sm">I accept the terms</span>
+        </label>
 
 
-          <div className="pt-2">
+        <div className="pt-2">
           <button
             type="submit"
             className="inline-flex h-10 items-center justify-center rounded-lg bg-black px-4 text-white shadow-sm transition-opacity disabled:opacity-50"
